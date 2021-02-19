118cea34f4af288cdeedaadea6ceec5c47557803
Al Avery <al.avery.dev@gmail.com>
2021-02-19 07:32:03 -0600

    working server with db and redis

--- sql/user_known_parts_of_speech.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_known_parts_of_speech (
-    id              bigserial PRIMARY KEY,
-    "user_id"       bigint,
-    part_of_speech  bigint,
-    CONSTRAINT      fk_parts_of_speech
-        FOREIGN KEY(part_of_speech)
-                REFERENCES part_of_speech(id)
-);
